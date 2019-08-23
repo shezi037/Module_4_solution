961e1876b4eb2b8c4de8e8f324e056bb960ac11f
shezi037 <43154915+shezi037@users.noreply.github.com>
2019-08-24 01:39:58 +0500

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,1 +1,8 @@
-
+(function(window) {
+    var byeSpeaker = new Obj();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window);
