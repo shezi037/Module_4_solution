d71dfe6713570011cc7da2d42a982eb4123043a7
shezi037 <43154915+shezi037@users.noreply.github.com>
2019-08-24 14:27:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ Hello Laura
 Good Bye Jim
 */
 
-(function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
-        } else {
-            helloSpeaker.speak(names[i]);
-        }
-    }
-})();
+(function (window) {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i in names) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter == 'j') {
+    window.byeSpeaker.speak(names[i]);
+  } else {
+    window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
