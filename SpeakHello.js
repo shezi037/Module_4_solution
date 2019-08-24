c03225ec8c99631e899b8835ea8121b4a84b3a95
shezi037 <43154915+shezi037@users.noreply.github.com>
2019-08-24 10:07:42 +0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,5 @@
 (function(window) {
-    var helloSpeaker = new Obj();
+    var helloSpeaker = ();
     var speakWord = "Hello";
     helloSpeaker.speak = function(name) {
         console.log(speakWord + " " + name);
