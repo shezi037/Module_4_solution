137d5539610a6b6156f83369d6012d52afffc31d
shezi037 <43154915+shezi037@users.noreply.github.com>
2019-08-24 14:13:56 +0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,8 +1,12 @@
-(function(window) {
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    helloSpeaker.speak = function(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.helloSpeaker = helloSpeaker;
+(function (window){
+ var helloSpeaker = {};
+ var speakWord = "Hello";
+    
+    
+helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+}
+ 
+ window.helloSpeaker = helloSpeaker   
+    
 })(window);
